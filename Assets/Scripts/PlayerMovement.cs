fdee3128ee472f0e0089861abeff6c6ea9f2d7a5
Vince Melia <vince@drifta.co.uk>
2020-03-07 18:34:28 +0000

    Partially fixed jumping bug.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
+        // If on the ground - reset velocity.
+        if (Controller.isGrounded)
+            _velocity.y = -2f;
+        
         var x = Input.GetAxis("Horizontal");
         var z = Input.GetAxis("Vertical");
 
@@ -19,15 +23,11 @@ public class PlayerMovement : MonoBehaviour
 
         Controller.Move(move * Speed * Time.deltaTime);
 
-        // Jump?
-        if (Input.GetButtonDown("Jump") && Controller.isGrounded)
+        // Jump.
+        if (Input.GetButtonDown("Jump"))    //ToDo: remove double+ jump.
             _velocity.y = Mathf.Sqrt(JumpHeight * -2f * Gravity);
 
         _velocity.y += Gravity * Time.deltaTime;
         Controller.Move(_velocity * Time.deltaTime);
-
-        // Landed - reset velocity.
-        if (Controller.isGrounded)
-            _velocity.y = -2f;
     }
 }
