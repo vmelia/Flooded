f490a3ec78c9a802bf325a225cfd16e92405cf87
Vince Melia <vince@drifta.co.uk>
2020-03-07 13:21:52 +0000

    Added test screen.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MouseLook : MonoBehaviour
+    {
+        public float Sensitivity = 300f;
+        public Transform PlayerBody;
+
+        private float _xRotation = 0f;
+
+        // Start is called before the first frame update
+        public void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Update is called once per frame
+        public  void Update()
+        {
+            var mouseX = Input.GetAxis("Mouse X") * Sensitivity * Time.deltaTime;
+            var mouseY = Input.GetAxis("Mouse Y") * Sensitivity * Time.deltaTime;
+
+            _xRotation -= mouseY;
+            _xRotation = Mathf.Clamp(_xRotation, -90f, +90f);
+
+            transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+
+            PlayerBody.Rotate(Vector3.up, mouseX);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController Controller;
+    public float Speed = 12f;
+    public float Gravity = -9.81f;
+
+    private Vector3 _velocity;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        var move = transform.right * x + transform.forward * z;
+
+        Controller.Move(move * Speed * Time.deltaTime);
+
+        _velocity.y += Gravity * Time.deltaTime;
+        Controller.Move(_velocity * Time.deltaTime);
+
+        if (Controller.transform.position.y <= 1.08f)
+            _velocity = new Vector3();
+    }
+}
