e0e21df4b2cc2ecf3144469b72704b0694402bdb
matt s <matt_sage@hotmail.co.uk>
2020-03-15 22:04:01 +0000

    Added NavMesh Tools and Player interactions with game objects

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MouseManager : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class MouseManager : MonoBehaviour
     public Texture2D target; // Cursor for clickable objects like the world
     public Texture2D doorway; // Cursor for doorways
     public Texture2D combat; // Cursor for combat actions
+    public EventVector3 OnClickEnvironment;
 
     // Update is called once per frame
     void Update()
@@ -21,13 +23,39 @@ public class MouseManager : MonoBehaviour
         if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
         {
             bool door = false;
+            bool item = false;
+
             if(hit.collider.gameObject.tag == "Doorway")
             {
                 Cursor.SetCursor(doorway, new Vector2 (16,16), CursorMode.Auto);
                 door = true;
             }
+            else if (hit.collider.gameObject.tag == "Item")
+            {
+                Cursor.SetCursor(combat, new Vector2(16,16), CursorMode.Auto);
+                item = true;
+            }
             else {
                 Cursor.SetCursor(target, new Vector2(16,16), CursorMode.Auto);
+            }
+
+            if(Input.GetMouseButtonDown(0)) 
+            {
+                if(door) {
+                    Transform doorway = hit.collider.gameObject.transform;
+                    OnClickEnvironment.Invoke(doorway.position);
+                    Debug.Log("DOOR");
+                }
+                else if(item)
+                {
+                    Transform itemPos = hit.collider.gameObject.transform;
+                    OnClickEnvironment.Invoke(itemPos.position);                    
+                    Debug.Log("ITEM");
+                }
+                else 
+                {
+                    OnClickEnvironment.Invoke(hit.point);
+                }
             }
         }
         else {
@@ -35,3 +63,6 @@ public class MouseManager : MonoBehaviour
         }
     }
 }
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    // private Animator anim;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+}
